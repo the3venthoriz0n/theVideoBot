9afb309dee584f38c731ce99c1cfa7189df779c0
the3venthoriz0n <indrewjones@gmail.com>
2023-04-11 14:50:30 -0700

    updpate gitignore

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from moviepy.editor import *
 
 
 
-# Set your API keys
+# Set your API keys, make a file called api_key.txt and paste api keys
 with open('api_key.txt', 'r') as f:
     OPENAI_API_KEY = f.readline().strip()
     PEXELS_API_KEY = f.readline().strip()
