09d492c4a1f40e212518cdf7b33ad55fff5a9bcb
sleepdreamrelaxagain <sleepdreamrelaxagain@gmail.com>
2023-04-12 17:15:22 -0400

    cleaned up top,  temp commented out pyttsx3

--- video_generator.py
+++ video_generator.py
@@ -1,5 +1,5 @@
-import pyttsx3
-import random, pyttsx3, os, openai, requests, re, shutil, textwrap, subprocess
+#import pyttsx3
+import random, os, openai, requests, re, shutil, textwrap, subprocess
 from moviepy.editor import * # import everything from moviepy
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from pydub import AudioSegment
