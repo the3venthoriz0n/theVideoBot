f35e50b8bbc8c80abfa276aa5421d3da9681ccc4
sleepdreamrelaxagain <sleepdreamrelaxagain@gmail.com>
2023-04-13 18:00:32 -0400

    changed the tts again

--- utils/tts.py
+++ utils/tts.py
@@ -3,14 +3,15 @@
 from pydub import AudioSegment
 import pyttsx3
 
-def text_to_speech(script):
+def text_to_speech(text):
     engine = pyttsx3.init()
     engine.setProperty('rate', 150)
     engine.setProperty('volume', 1)
     engine.setProperty('voice', engine.getProperty('voices')[0].id)
-    engine.save_to_file(script, 'output.mp3')
+    engine.save_to_file(text, 'output.mp3')
     engine.runAndWait()
     audio = AudioSegment.from_file("output.mp3", format="mp3")
     audio.export("output.mp3", format="mp3")
 
-text_to_speech("Hello, world!")
+text = "This is a test of the text to speech system"
+text_to_speech(text)
