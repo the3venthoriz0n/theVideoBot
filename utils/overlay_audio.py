26736475729aab4cfaaac5a8947991bbdb44837e
the3venthoriz0n <indrewjones@gmail.com>
2023-04-13 15:50:01 -0700

    Working on TTS

--- utils/overlay_audio.py
+++ utils/overlay_audio.py
@@ -1,20 +1,25 @@
 from pydub import AudioSegment
+# AudioSegment.ffmpeg = "C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe"
+# AudioSegment.converter = "C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe"
 
+def overlay_audio():
 
-
-def overlay_audio(sound1,sound2):
-
-    sound1 = AudioSegment.from_file("1.wav", format="wav")
-    sound2 = AudioSegment.from_file("2.wav", format="wav")
+    sound1 = AudioSegment.from_mp3("audio/coniferous-forest-142569.mp3")
+    sound2 = AudioSegment.from_mp3("audio/ttsOut.mp3")
 
     # sound1 6 dB louder
-    louder = sound1 + 6
+    #louder = sound1 + 6
 
     # Overlay sound2 over sound1 at position 0  (use louder instead of sound1 to use the louder version)
     overlay = sound1.overlay(sound2, position=0)
 
 
     # simple export
-    file_handle = overlay.export("output.mp3", format="mp3")
+    overlayedAudio = overlay.export("audio/finalAudio.mp3", format="mp3")
+    print("exported audio")
 
-return
+    return overlayedAudio
+
+
+
+overlay_audio()
