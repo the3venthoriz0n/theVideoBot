bf3cd9cb043e0f17ae4448cd2917f1579bdb15b6
Andrew K <11714005+the3venthoriz0n@users.noreply.github.com>
2024-03-13 14:40:06 -0700

    Update tts.py
    Remove profanity

--- utils/tts.py
+++ utils/tts.py
@@ -15,14 +15,11 @@ def text_to_speech(text):
 
 
     TEXT = text
-    #TEXT = 'hello how are you dash, you are looking nice today, hows the fam'
 
-    VOICE = "en-GB-SoniaNeural" #soft spoken UK chick
+    VOICE = "en-GB-SoniaNeural" #soft spoken UK
     #VOICE = "en-US-SteffanNeural" #soft spoken chill US bro
     #VOICE = "uk-UA-PolinaNeural" #Russian accent chick
     #VOICE = "zh-CN-XiaoxiaoNeural" #try this voice
-
-    #TEXT = "That's right! Some cats have a unique ability to sleep with their eyes partially or fully open, known as cat napping or watchful sleeping. This is due to their anatomy, as cats have a third eyelid, also called a nictitating membrane, which can cover their eyes while still allowing them to see their surroundings. It is believed that this ability to sleep with their eyes open is an adaptation to their natural hunting instincts, allowing them to quickly awaken and react to any potential prey or danger. However, not all cats can do this neat trick. Subscribe for more!"
 
 
     OUTPUT_FILE = "audio/ttsOut.mp3"
@@ -41,4 +38,4 @@ def text_to_speech(text):
     #engine.save_to_file(text, 'audio/ttsOut.wav')
     #engine.runAndWait()
 
-#text_to_speech("HELLO SLUT")
+#text_to_speech("HELLO THERE")
